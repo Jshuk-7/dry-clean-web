067cb2d61bcd010b71b53c4f6d75785aaa638cff
young_og <jessieshukeireh2001@gmail.com>
2023-11-08 19:52:35 -0500

    added home component + organized files + more css

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,12 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
         <div>
             <Header />
-            <h1>Curabitur Imperdiet</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                auctor pharetra lorem ac lobortis. Nam pulvinar tempus mauris,
-                eu facilisis urna vestibulum ac. In hac habitasse platea
-                dictumst. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Vivamus nulla lacus,
-                euismod eu tellus nec, dapibus tincidunt velit. Aenean volutpat
-                et massa et dapibus. Maecenas non ligula auctor massa mollis
-                consequat. Nullam venenatis commodo ex eget congue. Quisque
-                suscipit et enim ut tincidunt. Ut tristique in est eu molestie.
-                In hac habitasse platea dictumst. Interdum et malesuada fames ac
-                ante ipsum primis in faucibus. Curabitur imperdiet neque a purus
-                bibendum ultricies. Aenean quis pellentesque ipsum, eget
-                ultrices risus.
-            </p>
-            <h1>Maecenas Dolor Velit</h1>
-            <p>
-                Sed sed velit sodales, euismod nunc et, consequat est. Donec
-                congue ligula ac justo volutpat mattis. Aliquam iaculis suscipit
-                convallis. Vivamus consectetur ac sapien pretium pretium.
-                Suspendisse ut magna mauris. Donec mattis eros nibh. Aenean sed
-                vehicula nisi, ac facilisis urna. Nunc porttitor nunc vitae
-                velit pretium, id euismod lectus varius. In hac habitasse platea
-                dictumst. Quisque et lorem tristique, eleifend nulla eu, aliquam
-                nulla. Aenean facilisis libero dapibus finibus posuere. Etiam
-                mattis nunc at purus euismod, non ullamcorper purus feugiat.
-                Maecenas dolor velit, dapibus non ullamcorper ac, sodales a
-                dolor. Phasellus cursus velit a mi malesuada finibus. In
-                hendrerit consectetur odio, vitae bibendum arcu mollis eget. In
-                hac habitasse platea dictumst. Curabitur pretium tempor
-                venenatis.
-            </p>
-            <h1>Quisque Ac Egestas</h1>
-            <p>
-                Mauris in tortor justo. Donec ante urna, commodo vel convallis
-                vitae, eleifend ut justo. Duis sed mollis felis, nec fringilla
-                quam. Quisque sed fringilla urna. Pellentesque habitant morbi
-                tristique senectus et netus et malesuada fames ac turpis
-                egestas. Donec auctor non ante commodo porttitor. Suspendisse
-                placerat quis urna a elementum. Orci varius natoque penatibus et
-                magnis dis parturient montes, nascetur ridiculus mus. Duis sit
-                amet sapien at mi dapibus accumsan. Curabitur commodo lectus vel
-                gravida pharetra. Duis eget neque vel urna venenatis maximus.
-                Suspendisse potenti. Cras sed tellus sed lectus accumsan dapibus
-                ac et elit. Vivamus ac egestas mi. Vivamus molestie fermentum
-                tempus. Pellentesque elit elit, rutrum nec sollicitudin et,
-                semper aliquam risus. Praesent vitae facilisis ante, vel
-                placerat risus.
-            </p>
-            <h1>Maecenas Ultrices</h1>
-            <p>
-                Suspendisse ut faucibus neque. Vivamus vel libero venenatis,
-                ornare leo in, tempor sem. Cras tincidunt porta sodales.
-                Praesent fermentum nunc id dui accumsan tempus. Phasellus vel
-                velit feugiat, congue risus cursus, auctor magna. Ut nulla
-                dolor, pretium ac hendrerit sed, mollis sit amet odio. Donec
-                dapibus euismod dolor eu sollicitudin. Duis nec eros sit amet
-                massa dapibus mattis. Maecenas lobortis, ex et posuere lacinia,
-                velit ligula tempus ipsum, a tristique felis ex consequat nunc.
-                Sed vestibulum tortor ac sagittis vehicula. Vestibulum et
-                elementum massa. Pellentesque maximus lorem at accumsan sodales.
-                Vivamus et commodo erat, in facilisis nibh. Nulla faucibus vel
-                sapien eget luctus. Maecenas ultrices urna nec erat fermentum,
-                non auctor ante condimentum. Suspendisse bibendum felis at massa
-                convallis gravida.
-            </p>
-            <h1>Vivamus Faucibus</h1>
-            <p>
-                Etiam at mi ligula. Aenean sit amet velit non lorem porttitor
-                porttitor. Etiam eu metus blandit, fringilla metus mattis,
-                sodales libero. Proin vitae mattis nibh. Sed metus urna,
-                porttitor id vehicula et, convallis sit amet ante. Aliquam
-                congue odio nec est posuere commodo. Praesent faucibus urna eu
-                vestibulum lacinia. Vestibulum ante ipsum primis in faucibus
-                orci luctus et ultrices posuere cubilia curae; Nullam
-                condimentum commodo lorem, a fermentum diam ultricies dapibus.
-                Donec vulputate nisi ut nunc congue sodales. Nulla ullamcorper
-                pharetra ultricies. Etiam vitae tortor eu lectus ultricies
-                porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse et porta lacus, eu ornare arcu. Maecenas a finibus
-                metus, eu bibendum elit. Vivamus faucibus nibh in leo egestas,
-                porta vulputate augue efficitur. Donec lectus mauris, mollis sed
-                mi vel, aliquam consectetur odio.
-            </p>
+            <Home />
             <Footer />
         </div>
     );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        $3.75 Dry Clean & Shoe Repair
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home" className="nav-link">
+                                Home
+                            </Nav.Link>
+                            <NavDropdown
+                                title="Services"
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/2.1">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/2.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/2.3">
+                                    Something else
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#contact" className="nav-link">
+                                Contact Us
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
